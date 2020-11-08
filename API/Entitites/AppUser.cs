55257fb1e98f031757974d551895fe608a82dc3a
Luca Montanari <luca.montanari@libero.it>
2020-11-08 20:18:51 +0100

    End of section 2

---
+++ API/Entitites/AppUser.cs
@@ -0,0 +1,13 @@
+namespace API.Entitites
+{
+    
+    public class AppUser
+    {
+        
+        public int Id { get; set; }
+
+        public string UserNane { get; set; }
+        
+    }
+
+}

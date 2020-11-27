64b9eb3cb2c460e3f281b05a8a6e3414dd94eeea
Luca Montanari <luca.montanari@libero.it>
2020-11-27 18:30:22 +0100

    End of section  8

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -14,7 +14,6 @@ namespace API.Data
 
         }
 
-
         public DbSet<AppUser> Users { get; set; }
 
     }
--- API/Entitites/AppUser.cs
+++ API/Entitites/AppUser.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using API.Extensions;
+
 namespace API.Entitites
 {
     
@@ -11,6 +15,33 @@ namespace API.Entitites
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
 
     }
 

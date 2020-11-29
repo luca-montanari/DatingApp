c149800ef6c069c8205bc41110395c9bccc09563
Luca Montanari <luca.montanari@libero.it>
2020-11-29 20:17:58 +0100

    End of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+    selector: 'app-member-list',
+    templateUrl: './member-list.component.html',
+    styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+    members: Member[];
 
-  ngOnInit(): void {
-  }
+    constructor(private memberService: MembersService) { }
+
+    ngOnInit(): void {
+        this.loadMembers();
+    }
+
+    loadMembers(): void {
+        this.memberService.getMembers().subscribe(members => {
+            this.members = members;
+        });
+    }
 
 }
